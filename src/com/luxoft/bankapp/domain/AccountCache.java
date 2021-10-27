564ce54655b58a1b6b7c9cfeb78229893759ab90
Florin Mihalache <florinrm@users.noreply.github.com>
2021-10-27 22:15:28 +0300

    Added method

--- src/com/luxoft/bankapp/domain/AccountCache.java
+++ src/com/luxoft/bankapp/domain/AccountCache.java
@@ -7,14 +7,16 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class AccountCache {
-    private static final Map<String, AbstractAccount> accounts = new HashMap<>() {{
-        put(AccountType.CHECKING.name(),
+    private static final Map<String, AbstractAccount> accounts = new HashMap<>();
+
+    public static void loadCache() {
+        accounts.put(AccountType.CHECKING.name(),
                 (AbstractAccount) AccountFactory.createNewAccount(
                         AccountType.CHECKING, 0, 0));
-        put(AccountType.SAVING.name(),
+        accounts.put(AccountType.SAVING.name(),
                 (AbstractAccount) AccountFactory.createNewAccount(
                         AccountType.SAVING, 0, 0));
-    }};
+    }
 
     public static AbstractAccount createAccount(AccountType accountType) {
         return accounts.get(accountType.name()).clone();
--- src/com/luxoft/bankapp/tests/TestAccountCache.java
+++ src/com/luxoft/bankapp/tests/TestAccountCache.java
@@ -4,23 +4,30 @@ import com.luxoft.bankapp.domain.Account;
 import com.luxoft.bankapp.domain.AccountCache;
 import com.luxoft.bankapp.domain.CheckingAccount;
 import com.luxoft.bankapp.domain.SavingAccount;
-import com.luxoft.bankapp.factory.AccountFactory;
 import com.luxoft.bankapp.factory.AccountType;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertEquals;
 
 public class TestAccountCache {
     @Test
     public void testSavingAccount() {
+        AccountCache.loadCache();
         Account savingAccount = AccountCache.createAccount(AccountType.SAVING);
         assertThat(savingAccount, instanceOf(SavingAccount.class));
+        assertEquals(savingAccount.getBalance(), 0.0, 0);
+        assertEquals(savingAccount.getId(), 0);
     }
 
     @Test
     public void testCheckingAccount() {
+        AccountCache.loadCache();
         Account checkingAccount = AccountCache.createAccount(AccountType.CHECKING);
         assertThat(checkingAccount, instanceOf(CheckingAccount.class));
+        assertEquals(checkingAccount.getBalance(), 0.0, 0);
+        assertEquals(checkingAccount.getId(), 0);
+        assertEquals(((CheckingAccount) checkingAccount).getOverdraft(), 0.0, 0);
     }
 }
